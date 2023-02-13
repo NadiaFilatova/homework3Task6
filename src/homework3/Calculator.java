a1b25274242a01a1e819a1fea8fe4b9c558e3347
Nadia Filatova <nadiakocheluk09@gmail.com>
2023-02-13 14:50:14 +0200

    added calculator functions

--- src/homework3/Calculator.java
+++ src/homework3/Calculator.java
@@ -1,4 +1,36 @@
 package homework3;
 
+import java.util.Scanner;
+
 public class Calculator {
+    public static final String EQUAL = " = ";
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        Function.add(a, b);
+        Function.subtract(a, b);
+        Function.multiply(a, b);
+        Function.divide(a, b);
+    }
+
+    public static class Function {
+
+        public static void add(int a, int b) {
+            System.out.println(a + " + " + b + EQUAL + (a + b));
+        }
+
+        public static void subtract(int a, int b) {
+            System.out.println(a + " - " + b + EQUAL + (a - b));
+        }
+
+        public static void multiply(int a, int b) {
+            System.out.println(a + " * " + b + EQUAL + (a * b));
+        }
+
+        public static void divide(int a, int b) {
+            System.out.println(a + " / " + b + EQUAL + (a / b));
+        }
+    }
 }
